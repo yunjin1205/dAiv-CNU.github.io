83304c1da7bb4655482dd926ff078a0e949b2d5b
IRACK (CHAEUN) <IRACK000@gmail.com>
2023-10-27 02:44:22 +0900

    Feat: Goto Link

--- dist/src/main/contest/coding/shared.py
+++ dist/src/main/contest/coding/shared.py
@@ -16,13 +16,16 @@ async def set_iframe():
     psh_req = await window.fetch("https://pushoong.com/ask/7395560693")
     psh_html = ((await psh_req.text()).replace("115px", "0px")
                 .replace("//t1.daumcdn.net/kas/static/ba.min.js", "")
+                .replace("Kakao.init('4cca00b63eedb801abfc9952db0ee7a3');", "")
                 .replace("<head>", "<head>"
                                     "<base href=\"https://pushoong.com/\">"
                                     "<link href=\""+window.location.origin+"/dist/res/css/font.css\" rel=\"stylesheet\">")
                 .replace("<body>", "<body style=\"background-color: #fff;\">")
                 .replace("<div class=\"container\">", "<div class=\"container\" style=\"background-color: #fff;\">")
                 .replace("<div id=\"fullscreen-overlay\">", "<div id=\"fullscreen-overlay-disabled\" style=\"display:none;\">")
-                .replace("<div class=\"attach_border\" style=\"", "<div class=\"attach_border\" style=\"display:none;"))
+                .replace("<div class=\"attach_border\" style=\"", "<div class=\"attach_border\" style=\"display:none;")
+                .replace("input_border col", "col mt-1 ml-1\" onclick=\"window.open('https://pushoong.com/ask/7395560693', '_blank');")
+                .replace("id=\"ask_send\"", "id=\"ask_send_disabled\""))
 
     if pushoong:
         pushoong.open()
