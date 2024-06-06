05bd0f2d9c21a9e5aabdb06fdea151f33ab4cdab
BREW <IRACK000@gmail.com>
2024-06-07 00:11:22 +0900

    Fix: Fix Spacer

--- dist/src/main/judge/judge.py
+++ dist/src/main/judge/judge.py
@@ -16,14 +16,13 @@ async def insert_template(template_path: str, parent, index: int = -1, oncomplet
     result = await window.fetch("/dist/res/templates/"+template_path)
     insert_element(await result.text(), parent, index)
 
-    # footer 높이 계산
-    #footer_height = document.querySelector('footer').offsetHeight
+    # spacer 높이 조정
+    spacer = document.getElementById('footer-spacer')
+    if spacer:
+        body_height = document.body.offsetHeight
+        spacer.style.height = f"calc(100vh - {body_height}px)"
 
-    # iframe 높이 조정
-    #iframe = document.querySelector('.full-screen')
-    #iframe.style.height = f"{footer_height}px"
-
-    aio.run(oncomplete())
+    oncomplete()
 
 
 def insert_element(htmlstr: str, parent, index: int = -1):
@@ -36,19 +35,6 @@ def parse_html(htmlstr: str):
     template = document.createElement('template')
     template.innerHTML = htmlstr
     return template.content
-
-
-async def set_iframe():
-    judge = window.frames['online-judge'].document
-    judge_url = document.getElementsByName("online-judge")[0].attributes.data.value
-    judge_html = await (await window.fetch(judge_url)).text()
-    judge_html = judge_html.replace("<head>", f"<head>\n<base href=\"{judge_url}\">\n<link href=\""
-                                    + window.location.origin+"/dist/res/css/font.css\" rel=\"stylesheet\">")
-
-    if judge:
-        judge.open()
-        judge.write(judge_html)
-        judge.close()
 
 
 # Insert Templates
